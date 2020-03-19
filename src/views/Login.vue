637087cdba403ec451d9d14e3659a95b63870cad
moises <elnaufrago2009@gmail.com>
2020-03-18 23:55:01 -0500

    login front

---
+++ src/views/Login.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div class="col-8">
+            <!-- Card Login -->
+            <div class="card">
+                <div class="card-header">
+                    Contacta con Personas extraordinarias
+                </div>
+                <div class="card-body">
+
+                    <!-- Email -->
+                    <div class="d-inline-flex pt-3" style="width: 100%">
+                        <div style="width: 30%" class="text-sm-right">
+                            Email
+                        </div>
+                        <div style="width: 50%; box-sizing: content-box;">
+                            <div class="pl-2">
+                                <input type="email" class="form-control" placeholder="correo@gmail.com">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Password -->
+                    <div class="d-inline-flex pt-3" style="width: 100%">
+                        <div style="width: 30%" class="text-sm-right">
+                            Password
+                        </div>
+                        <div style="width: 50%; box-sizing: content-box;">
+                            <div class="pl-2">
+                                <input type="password" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Ingresar Unirme -->
+                    <div class="d-inline-flex pt-2" style="width: 100%">
+                        <div style="width: 30%" class="text-sm-right"></div>
+                        <div style="width: 25%; box-sizing: content-box;">
+                            <div class="pl-2">
+                                <button class="btn btn-outline-secondary btn-block">Ingresar</button>
+                            </div>
+                        </div>
+                        <div style="width: 25%; box-sizing: content-box;">
+                            <div class="pl-2">
+                                <button class="btn btn-outline-primary btn-block">Unirme</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Ingresar Anonimamente -->
+                    <div class="d-inline-flex pt-1" style="width: 100%">
+                        <div style="width: 30%" class="text-sm-right"></div>
+                        <div style="width: 50%; box-sizing: content-box;">
+                            <div class="pl-2">
+                                <button class="btn btn-outline-danger btn-block">Ingresar Anonimamente</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Ingresar Ambassador -->
+                    <div class="d-inline-flex pt-1" style="width: 100%">
+                        <div style="width: 30%" class="text-sm-right"></div>
+                        <div style="width: 50%; box-sizing: content-box;">
+                            <div class="pl-2">
+                                <button class="btn btn-outline-success btn-block">Ingresar Ambassador</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div><!-- end card-body -->
+            </div><!-- end card -->
+        </div><!-- end col-10 -->
+    </div>
+</template>
